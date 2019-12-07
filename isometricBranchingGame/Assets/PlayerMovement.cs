616ce88b3a7cf15009dd2352e4bd24a0306d1562
mpalmer <mpalmer0028@gmail.com>
2019-12-07 13:22:15 -0500

    Add player cube and basic movement script
    Cube added for test player, simple movement script added to start testing/coding interactions

---
+++ isometricBranchingGame/Assets/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public float MovementSpeed = 4f;
+
+    Vector3 forward, right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make forward of player match the forward of the camera 
+        forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward = Vector3.Normalize(forward);
+
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            Move();
+        }
+    }
+
+
+    private void Move()
+    {
+
+        var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        var rightMovement = right * MovementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        var forwardMovement = forward * MovementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        // Rotate player to the correct direction 
+        var heading = Vector3.Normalize(rightMovement + forwardMovement);
+        transform.forward = heading;
+
+        // Move player
+        transform.position += rightMovement + forwardMovement;
+    }
+}
