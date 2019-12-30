c01e8f3584934f365aae56ce8a27df27f1fd033d
mpalmer <mpalmer0028@gmail.com>
2019-12-29 19:41:47 -0500

    Add basic door with trigger

---
+++ isometricBranchingGame/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    Animator animator;
+    public GameObject player;
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            animator.SetBool("IsOpen", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            animator.SetBool("IsOpen", false);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //animator.SetTrigger("openDoorTop");
+            animator.SetBool("IsOpen", !animator.GetBool("IsOpen"));
+        }
+    }
+}
