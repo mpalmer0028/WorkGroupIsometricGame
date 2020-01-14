54afdde2faa8fc19a007fefc8745fba9159e270a
mpalmer <mpalmer0028@gmail.com>
2020-01-14 16:09:13 -0500

    Add Cells scene

---
+++ isometricBranchingGame/Assets/Scripts/PassCollisionToParent.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+/// <summary>
+/// This script passes the collisions of this object to a parent script
+/// </summary>
+public class PassCollisionToParent : MonoBehaviour
+{
+    /// <summary>
+    /// Script on the parent that will handle collisions
+    /// </summary>
+    private IParentWithChildColliders parentScript;
+
+     void Start()
+    {
+        // Get the script on the parent that will handle collisions
+        parentScript = transform.parent.GetComponent<IParentWithChildColliders>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        parentScript.HandleOnCollisionEnterFromChild(name, collider);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        parentScript.HandleOnCollisionExitFromChild(name, collider);
+    }
+}
---
+++ isometricBranchingGame/Assets/test.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //void OnTriggerEnter(Collision collision)
+    //{
+    //    Debug.Log(collision.gameObject.name);
+    //}
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    Debug.Log("entered");
+    //}
+
+    //// stayCount allows the OnTriggerStay to be displayed less often
+    //// than it actually occurs.
+    //private float stayCount = 0.0f;
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    Debug.Log("staying");
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    Debug.Log("exit");
+    //}
+}
