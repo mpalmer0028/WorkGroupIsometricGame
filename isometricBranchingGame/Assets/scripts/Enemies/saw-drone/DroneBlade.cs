b6d2028c962f20b05ad9d3049778a5aa289dfdb0
paul-king <paul.kingworks@gmail.com>
2020-01-22 18:03:23 -0500

    Rotating Saw Blades

---
+++ isometricBranchingGame/Assets/scripts/Enemies/saw-drone/DroneBlade.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneBlade : MonoBehaviour
+{
+    void Update()
+    {
+      transform.Rotate(new Vector3(0f, 200f, 0f) * Time.deltaTime);
+    }
+}
