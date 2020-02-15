9ddb59ceab02f252bc7f9cf863356aa854db1e12
mpalmer <mpalmer0028@gmail.com>
2020-02-15 16:48:34 -0500

    Add blend tree for walking animations

--- isometricBranchingGame/Assets/Scripts/Player/PlayerMovement.cs
+++ isometricBranchingGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 forward, right;
 
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         // Make forward of player match the forward of the camera 
         forward = Camera.main.transform.forward;
         forward.y = 0;
@@ -37,6 +39,11 @@ public class PlayerMovement : MonoBehaviour
         var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         var rightMovement = right * movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
         var forwardMovement = forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+
+        // todo: make these floats update based on movement relative to local rotation
+        // can't setup till character movement is independent of which direction the character is facing
+        animator.SetFloat("VelocityY", Input.GetAxis("Vertical"));
+        animator.SetFloat("VelocityX", Input.GetAxis("Horizontal"));
 
         // Rotate player to the correct direction 
         var heading = Vector3.Normalize(rightMovement + forwardMovement);
