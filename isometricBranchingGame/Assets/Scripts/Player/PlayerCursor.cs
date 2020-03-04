865eb48f3fefbf923c86e73413180ec4c0f7356e
mpalmer <mpalmer0028@gmail.com>
2020-03-03 22:33:48 -0500

    Add cursor and movement script

---
+++ isometricBranchingGame/Assets/Scripts/Player/PlayerCursor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCursor : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject cursor;
+    [SerializeField]
+    public float slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cursor = Instantiate(cursor, new Vector3(0f, 0f, 0f), Quaternion.identity);
+        GetComponent<PlayerMovement>().cursor = cursor;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        var plane = new Plane(Vector3.down, gameObject.transform.position.y);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        float distanceToPlane;
+        if(plane.Raycast(ray,out distanceToPlane))
+        {
+            cursor.transform.position = ray.GetPoint(distanceToPlane);
+        }
+    }
+}
--- isometricBranchingGame/Assets/Scripts/Player/PlayerMovement.cs
+++ isometricBranchingGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,18 +8,30 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     public float movementSpeed = 8f;
 
-    Vector3 forward, right;
+    [SerializeField]
+    public float rotateSpeed = 8f;
 
-    public Animator animator;
+    public Vector3 forward, right;
+    public GameObject cursor;
+
+    private Animator animator;
+    private PlayerCursor playerCursor;
+    private Rigidbody rigidbody;
+    
+    private int i = 0;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        playerCursor = GetComponent<PlayerCursor>();
+        rigidbody = GetComponent<Rigidbody>();
+
+        cursor = playerCursor.cursor;
         // Make forward of player match the forward of the camera 
         forward = Camera.main.transform.forward;
         forward.y = 0;
         forward = Vector3.Normalize(forward);
-
+        
         right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
 
     }
@@ -30,24 +43,56 @@ public class PlayerMovement : MonoBehaviour
         {
             Move();
         }
+
+
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = cursor.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = rotateSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+
+        transform.rotation = Quaternion.LookRotation(newDirection);
+
+
+
+        // todo: make these floats update based on movement relative to local rotation
+        // can't setup till character movement is independent of which direction the character is facing
+        var localVelocity = transform.InverseTransformDirection(rigidbody.velocity);
+
+        //if (i == 200)
+        //{
+        //    Debug.Log(transform.rotation);
+        //    Debug.Log(cursor.transform.position);
+        //    // Lock velocities to 1,0,-1
+        //    //var vz = localVelocity.z < 0 ? -1 : (localVelocity.z > 0 ? 1 : 0);
+        //    //var vx = localVelocity.x < 0 ? -1 : (localVelocity.x > 0 ? 1 : 0);
+        //    //animator.SetFloat("VelocityZ", 0);
+        //    //animator.SetFloat("VelocityX", 0);
+        //    i = 0;
+        //}
+        //else
+        //{
+        //    i++;
+        //}
+
+
+        //Input.GetAxis("Horizontal")
+
     }
 
 
     private void Move()
-    {
-
-        var direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        var rightMovement = right * movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
-        var forwardMovement = forward * movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
-
-        // todo: make these floats update based on movement relative to local rotation
-        // can't setup till character movement is independent of which direction the character is facing
-        animator.SetFloat("VelocityY", Input.GetAxis("Vertical"));
-        animator.SetFloat("VelocityX", Input.GetAxis("Horizontal"));
+    {        
+        var direction = new Vector3(Input.GetAxis("HorizontalKey"), 0, Input.GetAxis("VerticalKey"));
+        var rightMovement = right * movementSpeed * Time.deltaTime * Input.GetAxis("HorizontalKey");
+        var forwardMovement = forward * movementSpeed * Time.deltaTime * Input.GetAxis("VerticalKey");
 
         // Rotate player to the correct direction 
-        var heading = Vector3.Normalize(rightMovement + forwardMovement);
-        transform.forward = heading;
+        //var heading = Vector3.Normalize(rightMovement + forwardMovement);
 
         // Move player
         transform.position += rightMovement + forwardMovement;
